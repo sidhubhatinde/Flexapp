b32569c5975369b3e96f4e71f2dfa5eaea3c15bc
Ammar Haider <ammarhaider2303@gmail.com>
2025-04-26 12:58:06 +0500

    updated tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#108a00',
+        primaryHover: '#0c6c00',
+        textPrimary: '#1f2937',
+        textSecondary: '#4b5563',
+        borderLight: '#e5e7eb',
+        accent: '#3b82f6',
+        error: '#ef4444',
+        success: '#22c55e',
+        warning: '#f59e0b',
+      }
+    },
   },
   plugins: [],
 }
