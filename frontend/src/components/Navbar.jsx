69f0b7443d6f98db2a65ee96e4bb1ccf61a5706c
Muhammad Yousaf Khan <i228797@lhr.nu.edu.pk>
2025-05-02 12:13:10 +0500

    just  make the nav-text smaller so it could fit and started work on login signup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,20 +8,20 @@ function Navbar() {
         <div className="flex items-center space-x-11">
           <ul className="flex space-x-9 items-center">
             <li className="text-5xl font-bold text-black font-permanent-marker"><Link to=''>Flex Work</Link></li>
-            <li className="text-3xl"><Link to=''>Find talent</Link></li>
-            <li className="text-3xl"><Link to=''>Find work</Link></li>
-            <li className="text-3xl"><Link to=''>Why Flexwork</Link></li>
-            <li className="text-3xl"><Link to=''>What's new</Link></li>
-            <li className="text-3xl"><Link to=''>Pricing</Link></li>
+            <li className="text-2xl"><Link to=''>Find talent</Link></li>
+            <li className="text-2xl"><Link to=''>Find work</Link></li>
+            <li className="text-2xl"><Link to=''>Why Flexwork</Link></li>
+            <li className="text-2xl"><Link to=''>What's new</Link></li>
+            <li className="text-2xl"><Link to=''>Pricing</Link></li>
           </ul>
         </div>
         <div className="flex space-x-11">
           <ul className="flex space-x-9 items-center">
             <li>
-              <Link to='' className="text-3xl hover:text-primaryHover">Log in</Link>
+              <Link to='' className="text-2xl hover:text-primaryHover">Log in</Link>
             </li>
             <li>
-              <Link to='' className="text-3xl text-white bg-primary rounded-2xl px-8 py-5 text-blue-600 hover:text-blue-800">Sign up</Link>
+              <Link to='' className="text-2xl text-white bg-primary rounded-2xl px-8 py-5 text-blue-600 hover:text-blue-800">Sign up</Link>
             </li>
           </ul>
         </div>
