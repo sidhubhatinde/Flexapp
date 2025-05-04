8ee694a79ea246f4df0c8db52289d61fe7e2189c
Ammar Haider <ammarhaider2303@gmail.com>
2025-05-04 23:47:42 +0500

    conditional rendering navbar on bases of users type

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { CgProfile } from "react-icons/cg";
 
 function Navbar() {
+  
+  const isAuthenticated = true;
+  const user = {
+    role:"client"
+  }
+
   return (
     <nav className="bg-white shadow-md py-8 px-10">
-      <div className="flex justify-between items-center">
+            {isAuthenticated ? (
+        user.role === 'client' ? (      <div className="flex justify-between items-center">
+          <div className="flex items-center space-x-11">
+            <ul className="flex space-x-9 items-center">
+              <li className="text-5xl font-bold text-black font-permanent-marker"><Link to='/'>Flex Work</Link></li>
+              <li className="text-3xl hover:text-primaryHover"><Link to=''>Post Job</Link></li>
+              <li className="text-3xl hover:text-primaryHover"><Link to=''>Proposals</Link></li>
+              <li className="text-3xl hover:text-primaryHover"><Link to=''>All Jobs</Link></li>
+              <li className="text-3xl hover:text-primaryHover"><Link to=''>Messages</Link></li>
+            </ul>
+          </div>
+          <div className="flex space-x-11">
+            <ul className="flex space-x-9 items-center">
+              <li>
+                <Link to='' className="text-3xl text-white bg-primary rounded-2xl px-8 py-5 hover:bg-primaryHover">Log out</Link>
+              </li>
+              <li>
+                <CgProfile className='size-16'/>
+              </li>
+            </ul>
+          </div>
+        </div>
+        ) : (
+          <div className="flex justify-between items-center">
+          <div className="flex items-center space-x-11">
+            <ul className="flex space-x-9 items-center">
+              <li className="text-5xl font-bold text-black font-permanent-marker"><Link to=''>Flex Work</Link></li>
+              <li className="text-3xl hover:text-primaryHover"><Link to='/find-talent'>Find Work</Link></li>
+              <li className="text-3xl hover:text-primaryHover"><Link to='/find-work'>Saved Jobs</Link></li>
+              <li className="text-3xl hover:text-primaryHover"><Link to='/why-flexwork'>Proposals</Link></li>
+              <li className="text-3xl hover:text-primaryHover"><Link to=''>Messages</Link></li>
+            </ul>
+          </div>
+          <div className="flex space-x-11">
+            <ul className="flex space-x-9 items-center">
+              <li>
+                <Link to='' className="text-3xl text-white bg-primary rounded-2xl px-8 py-5 hover:bg-primaryHover">Log out</Link>
+              </li>
+              <li>
+                <CgProfile className='size-16'/>
+              </li>
+            </ul>
+          </div>
+        </div>
+        )
+      ) : (
+        <div className="flex justify-between items-center">
         <div className="flex items-center space-x-11">
           <ul className="flex space-x-9 items-center">
             <li className="text-5xl font-bold text-black font-permanent-marker"><Link to=''>Flex Work</Link></li>
@@ -26,6 +79,7 @@ function Navbar() {
           </ul>
         </div>
       </div>
+      )}
     </nav>
   )
 }
--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,19 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
-<<<<<<< HEAD
+
 import FindWork from "../pages/findwork/FindWork";
 import JobProposal from "../pages/findwork/JobProposal";
 import FindTalent from "../pages/findtalent/FindTalent";
 import JobPost from "../pages/findtalent/JobPost";
 import WhyFlexwork from "../pages/whyflexwork/WhyFlexwork";
-=======
-import Login from "../pages/login";
-import Signup from "../pages/Signup";
-
-
-
->>>>>>> ce129d6121cf800ce9081455b8d3ae51ccb3d0c4
 
 const router = createBrowserRouter([
     {
@@ -26,7 +19,6 @@ const router = createBrowserRouter([
                 element: <Home />
             },
             {
-<<<<<<< HEAD
                 path:'/find-work',
                 element:<FindWork/>,
             },
@@ -47,15 +39,6 @@ const router = createBrowserRouter([
                 element:<WhyFlexwork/>
             }
 
-=======
-                path: 'login',
-                element: <Login />
-            },
-            {
-                path: 'signup',
-                element: <Signup />
-            }
->>>>>>> ce129d6121cf800ce9081455b8d3ae51ccb3d0c4
         ]
     }
 ])
